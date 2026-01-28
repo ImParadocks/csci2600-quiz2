f5de61d3c1e7be714728211cb3289df5dd8967ac
ImParadocks <77418462+ImParadocks@users.noreply.github.com>
2026-01-28 12:57:43 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {  
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
-	System.out.println("Hello, World!");  
+		System.out.println("Hello, World!");  
         int x = 1;
         
         for (int i = 1; i<9 ; i++){
